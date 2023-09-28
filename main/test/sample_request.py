b0f714b406d259461a480071cb37aa9413ec3598
Sourav Kumar Bose <sobose@linkedin.com>
2023-09-28 10:45:23 +0530

    added bw check

--- main/test/sample_request.py
+++ main/test/sample_request.py
@@ -1,7 +1,7 @@
 import requests
 
-url = "http://localhost:5000/generate_story"
-# url = "http://74.235.105.189:5000/generate_story"
+# url = "http://localhost:5000/generate_story"
+url = "http://74.235.105.189:5000/generate_story"
 
 data = {
     "prompt": "Write a story about a lion and rabbit in 100 words.",
@@ -12,6 +12,8 @@ response = requests.post(url, json=data)
 print (response)
 
 if response.status_code == 200:
+    download_speed = len(response.content) / (response.elapsed.total_seconds() * 1024 * 1024)
+    print(f"Download speed: {round(download_speed, 2)} mb/s")
     with open("story.wav", "wb") as f:
         f.write(response.content)
         print("Story audio saved successfully!")
